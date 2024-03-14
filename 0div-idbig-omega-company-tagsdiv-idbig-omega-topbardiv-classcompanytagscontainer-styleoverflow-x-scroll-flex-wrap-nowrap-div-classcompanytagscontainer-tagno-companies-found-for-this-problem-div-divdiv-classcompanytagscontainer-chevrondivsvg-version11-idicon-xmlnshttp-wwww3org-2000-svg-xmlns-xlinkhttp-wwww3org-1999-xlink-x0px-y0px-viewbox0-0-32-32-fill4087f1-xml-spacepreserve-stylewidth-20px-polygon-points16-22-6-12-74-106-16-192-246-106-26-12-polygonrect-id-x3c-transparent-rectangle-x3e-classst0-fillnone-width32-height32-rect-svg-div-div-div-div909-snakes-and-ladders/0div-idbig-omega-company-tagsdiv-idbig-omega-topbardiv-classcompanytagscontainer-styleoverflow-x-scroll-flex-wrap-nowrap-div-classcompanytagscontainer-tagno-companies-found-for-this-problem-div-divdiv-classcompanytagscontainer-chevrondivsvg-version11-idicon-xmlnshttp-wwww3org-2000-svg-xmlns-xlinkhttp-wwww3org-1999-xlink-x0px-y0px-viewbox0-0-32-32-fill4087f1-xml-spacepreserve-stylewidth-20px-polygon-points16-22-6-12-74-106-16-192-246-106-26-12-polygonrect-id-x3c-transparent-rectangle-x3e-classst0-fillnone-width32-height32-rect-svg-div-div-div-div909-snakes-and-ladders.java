d930bb79f2587c53d8ce6c04dfb97f7fc872040d
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2024-03-14 15:32:26 -0400

    Time: 14 ms (22.62%), Space: 44.9 MB (37.89%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div909-snakes-and-ladders/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div909-snakes-and-ladders.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div909-snakes-and-ladders/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div909-snakes-and-ladders.java
@@ -1,34 +1,39 @@
 class Solution {
+    
     public int snakesAndLadders(int[][] board) {
-        int n = board.length;
-        Pair<Integer, Integer>[] cells = new Pair[n * n + 1];
-        int label = 1;
-        Integer[] columns = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            columns[i] = i;
+        int n=board.length;
+        Pair<Integer,Integer>[] cells=new Pair[n*n+1];
+        
+        Integer[] columns=new Integer[n];
+        for(int i=0;i<n;i++){
+            columns[i]=i;
         }
-        for (int row = n - 1; row >= 0; row--) {
-            for (int column : columns) {
-                cells[label++] = new Pair<>(row, column);
+        int label=1;
+        for(int row=n-1;row>=0;row--){
+            for(int column:columns){
+                cells[label++]=new Pair<>(row,column);
             }
             Collections.reverse(Arrays.asList(columns));
         }
-        int[] dist = new int[n * n + 1];
-        Arrays.fill(dist, -1);
-        Queue<Integer> q = new LinkedList<Integer>();
-        dist[1] = 0;
-        q.add(1);
-        while (!q.isEmpty()) {
-            int curr = q.remove();
-            for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
-                int row = cells[next].getKey(), column = cells[next].getValue();
-                int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1) {
-                    dist[destination] = dist[curr] + 1;
-                    q.add(destination);
+        
+        int[] dist=new int[n*n+1];
+        Arrays.fill(dist,-1);
+        Queue<Integer> queue= new LinkedList<>();
+        queue.add(1);
+        dist[1]=0;
+        while(!queue.isEmpty()){
+            int cur=queue.remove();
+            for(int next=cur+1;next<=Math.min(cur+6,n*n);next++){
+                int row=cells[next].getKey();
+                int col=cells[next].getValue();
+                int destination = board[row][col]!=-1?board[row][col]:next;
+                
+                if(dist[destination]==-1){
+                    dist[destination]=dist[cur]+1;
+                    queue.add(destination);
                 }
             }
         }
-        return dist[n * n];
+        return dist[n*n];
     }
 }
