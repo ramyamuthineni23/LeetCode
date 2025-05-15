87abaaf9dbe896dc86c32781ffcbbb056e39e86d
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-05-15 00:10:28 -0400

    Time: 1 ms (99.15%), Space: 43.5 MB (44.66%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -0,0 +1,30 @@
+class Solution {
+    private Integer maxWidth = 0;
+    private HashMap<Integer, Integer> firstColIndexTable;
+
+    protected void DFS(TreeNode node, Integer depth, Integer colIndex) {
+        if (node == null)
+            return;
+        // Initialize the value, for the first seen colIndex per level
+        if (!firstColIndexTable.containsKey(depth)) {
+            firstColIndexTable.put(depth, colIndex);
+        }
+        Integer firstColIndex = firstColIndexTable.get(depth);
+
+        maxWidth = Math.max(this.maxWidth, colIndex - firstColIndex + 1);
+
+        // Preorder DFS. Note: it is important to put the priority on the left child
+        DFS(node.left, depth + 1, 2 * colIndex);
+        DFS(node.right, depth + 1, 2 * colIndex + 1);
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+        // The table contains the first col_index for each level
+        this.firstColIndexTable = new HashMap<Integer, Integer>();
+
+        // Start with depth = 0, and colIndex = 0
+        DFS(root, 0, 0);
+
+        return this.maxWidth;
+    }
+}
