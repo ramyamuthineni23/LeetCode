74c28c7324a1a37469da6260ed6ffaeb852c3be2
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-05-15 00:13:48 -0400

    Time: 0 ms (100%), Space: 42.6 MB (99.87%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,30 +1,23 @@
 class Solution {
-    private Integer maxWidth = 0;
-    private HashMap<Integer, Integer> firstColIndexTable;
-
-    protected void DFS(TreeNode node, Integer depth, Integer colIndex) {
-        if (node == null)
-            return;
-        // Initialize the value, for the first seen colIndex per level
-        if (!firstColIndexTable.containsKey(depth)) {
-            firstColIndexTable.put(depth, colIndex);
-        }
-        Integer firstColIndex = firstColIndexTable.get(depth);
-
-        maxWidth = Math.max(this.maxWidth, colIndex - firstColIndex + 1);
-
-        // Preorder DFS. Note: it is important to put the priority on the left child
-        DFS(node.left, depth + 1, 2 * colIndex);
-        DFS(node.right, depth + 1, 2 * colIndex + 1);
-    }
-
+    LinkedList<Integer> leftView = new LinkedList<>();
+    int maxDep = 1;
     public int widthOfBinaryTree(TreeNode root) {
-        // The table contains the first col_index for each level
-        this.firstColIndexTable = new HashMap<Integer, Integer>();
-
-        // Start with depth = 0, and colIndex = 0
-        DFS(root, 0, 0);
-
-        return this.maxWidth;
+        if (root==null){
+            return 0;
+        }
+        traverse(root, 1, 1);
+        return maxDep;
+    }
+    public void traverse(TreeNode root, int id, int depth){
+        if (root==null){
+            return;
+        }
+        if (leftView.size()==depth-1){
+            leftView.addLast(id);
+        } else {
+            maxDep = Math.max(maxDep, (id-leftView.get(depth-1)+1));
+        }
+        traverse(root.left, 2*id, depth+1);
+        traverse(root.right, 2*id+1, depth+1);
     }
 }
