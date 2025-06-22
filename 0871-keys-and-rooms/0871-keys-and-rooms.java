12bd14c1c83bdd5576e0a6da9ea45fb2a7bebff8
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-06-22 17:33:56 -0400

    Time: 3 ms (22.05%), Space: 44.4 MB (62.29%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int n= rooms.size();
-        boolean[] visited=new boolean[n];
-        Queue<Integer> queue=new LinkedList<>();
-        visited[0]=true;
-        queue.add(0);
+        int size=rooms.size();
+        boolean visited[] = new boolean [size];
 
+        Queue<Integer> queue=new LinkedList<>();
+        queue.add(0);
         while(!queue.isEmpty()){
-            int curr=queue.remove();
-            List<Integer> adj=rooms.get(curr);
-            for(int adjRoom:adj){
-                if(!visited[adjRoom]){
-                    queue.add(adjRoom);
-                    visited[adjRoom]=true;
+            int node=queue.poll();
+            visited[node]=true;
+
+            for(int neigh:rooms.get(node)){
+                if(!visited[neigh]){
+                    queue.add(neigh);
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            if(!visited[i])
-            return false;
+
+        for(boolean visit:visited){
+            if(!visit)
+                return false;
         }
         return true;
     }
