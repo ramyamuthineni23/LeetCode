45edc4936ee8975ed9446287560400979ada3315
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-09 19:14:27 -0400

    Time: 14 ms (67.81%), Space: 46.1 MB (82.67%) - LeetHub

--- 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.java
@@ -1,13 +1,15 @@
-class Solution {
+public class Solution {
     public int maxSumDivThree(int[] nums) {
-        int[] dp = new int[3];
+        int[] dp = new int[3]; // dp[i] stores the max sum with remainder i
+
         for (int num : nums) {
-            int[] temp = dp.clone();
+            int[] temp = dp.clone(); // Copy current dp state to avoid overwriting
             for (int i = 0; i < 3; i++) {
                 int sum = temp[i] + num;
                 dp[sum % 3] = Math.max(dp[sum % 3], sum);
             }
         }
-        return dp[0];
+
+        return dp[0]; // maximum sum divisible by 3
     }
 }
