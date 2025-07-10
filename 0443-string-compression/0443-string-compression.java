c8d2cab7c880cf1df93396693fa308d19cdd0432
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-10 01:28:41 -0400

    Time: 1 ms (99.47%), Space: 43.8 MB (88.98%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,20 +1,18 @@
 class Solution {
     public int compress(char[] chars) {
-        int i=0, n=chars.length;
-        int res=0;
-        
-        while(i<n){
-            int groupLength=1;
-            if(res<n){
-            chars[res++]=chars[i];
-            while(i+groupLength<n && chars[i+groupLength]==chars[i]){
+        int i = 0, res = 0;
+        while (i < chars.length) {
+            int groupLength = 1;
+            while (i + groupLength < chars.length && chars[i + groupLength] == chars[i]) {
                 groupLength++;
             }
-            if(groupLength>1){
-            for(char ch:String.valueOf(groupLength).toCharArray()){
-                chars[res++]=ch;
-            }}}
-            i=i+groupLength;
+            chars[res++] = chars[i];
+            if (groupLength > 1) {
+                for (char c : Integer.toString(groupLength).toCharArray()) {
+                    chars[res++] = c;
+                }
+            }
+            i += groupLength;
         }
         return res;
     }
