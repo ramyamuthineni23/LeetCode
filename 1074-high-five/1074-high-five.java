6be8ea12fa6929de19a14109d399179fa729a8c6
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-09 23:36:23 -0400

    Time: 6 ms (37.05%), Space: 45.2 MB (15.19%) - LeetHub

--- 1074-high-five/1074-high-five.java
+++ 1074-high-five/1074-high-five.java
@@ -8,17 +8,19 @@ class Solution {
             int id = item[0];
             int score = item[1];
             if (!allScores.containsKey(id))
-                // max heap
-                allScores.put(id, new PriorityQueue<>((a,b) -> b - a));
-            // Add score to the max heap
+                allScores.put(id, new PriorityQueue<>());
+            // insert the score in the min heap
             allScores.get(id).add(score);
+            // remove the minimum element from the min heap in case the size of the min heap exceeds 5 
+            if (allScores.get(id).size() > this.K)
+                allScores.get(id).poll();
         }
 
         List<int[]> solution = new ArrayList<>();
 
         for (int id : allScores.keySet()) {
             int sum = 0;
-            // obtain the top k scores (k = 5)
+            // min heap contains the top 5 scores
             for (int i = 0; i < this.K; ++i)
                 sum += allScores.get(id).poll();
             solution.add(new int[] {id, sum / this.K});
