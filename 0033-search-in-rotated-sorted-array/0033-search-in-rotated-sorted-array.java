bb6b4604c77c47cea8c9bb2bbf2c9fc089700d8b
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-10 02:11:15 -0400

    Time: 0 ms (100%), Space: 42.1 MB (67.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,40 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        if(n==1)
-        {
-            if(nums[0]==target){
-                return 0;
-            }
-            return -1;
-        }
-        
+        int n = nums.length;
+        int left = 0, right = n - 1;
 
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]==target)
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Case 1: find target
+            if (nums[mid] == target) {
                 return mid;
-            if(nums[left]<=nums[mid]){
-
-                if(nums[left]<=target && target<=nums[mid]){
-                    right=mid-1;
-                }
-                else{
-                    left=mid+1;
+            }
+            // Case 2: subarray on mid's left is sorted
+            else if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
-            else{
-                if(nums[mid]<=target && target<=nums[right]){
-                    left=mid+1;
-                }
-                else{
-                    right=mid-1;
+            // Case 3: subarray on mid's right is sorted
+            else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             }
-        
         }
+
         return -1;
     }
 }
