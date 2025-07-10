065cd6a725a665fa4f1fe98bee6394315f5af436
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-10 02:54:25 -0400

    Time: 0 ms (100%), Space: 41.7 MB (79.04%) - LeetHub

--- 1283-reformat-date/1283-reformat-date.java
+++ 1283-reformat-date/1283-reformat-date.java
@@ -1,13 +1,31 @@
 class Solution {
-        private static final List<String> MONTHS = List.of("", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+    public String reformatDate(String date) {
+         StringBuilder sb = new StringBuilder();
+         sb.append(date.substring(date.length() - 4)).append("-");
+         String m = date.length() == 13 ? date.substring(5, 8) : date.substring(4, 7);
 
-public String reformatDate(String date) {
-	var parts = date.split(" ");
-	return new StringBuilder(parts[2])
-					 .append("-")
-					 .append(String.format("%02d", MONTHS.indexOf(parts[1])))
-					 .append("-")
-					 .append(String.format("%02d", Integer.parseInt(parts[0].substring(0, parts[0].length() - 2))))
-					 .toString();
+         switch(m){
+             case "Jan": sb.append("01-"); break;
+             case "Feb": sb.append("02-"); break;
+             case "Mar": sb.append("03-"); break;
+             case "Apr": sb.append("04-"); break;
+             case "May": sb.append("05-"); break;
+             case "Jun": sb.append("06-"); break;
+             case "Jul": sb.append("07-"); break;
+             case "Aug": sb.append("08-"); break;
+             case "Sep": sb.append("09-"); break;
+             case "Oct": sb.append("10-"); break;
+             case "Nov": sb.append("11-"); break;
+             case "Dec": sb.append("12-"); break;
+         }
+
+         if(date.length() < 13){
+           sb.append("0");
+           sb.append(date.substring(0,1));
+         }else{
+           sb.append(date.substring(0,2));
+         }
+
+         return sb.toString();
     }
 }
