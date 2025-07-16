d287439d4ba5e5b4dfdeb9b5228f69f59f1509d8
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-16 14:18:07 -0500

    Time: 2 ms (97.34%), Space: 42 MB (51.45%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,21 +1,31 @@
 class Solution {
-
     public int nthUglyNumber(int n) {
-        TreeSet<Long> uglyNumbersSet = new TreeSet<>(); // TreeSet to store potential ugly numbers
-        uglyNumbersSet.add(1L); // Start with 1, the first ugly number
-        // TreeSet automatically sorts elements in ascending order and does not
-        // allow duplicate entries, just like a HashSet in python
+        //The logic is any ugly number can be represented as pow(2,a)*pow(3,b)*pow(5,c)
+        //So if x is an ugly number, whatever numbers we will be getting by multiplying 2,3 or 5 in it
+        //Will also be an ugly number
+        //So we are keeping 3 variables, and taking the minimum from them, and moving ahead
+        
+        int[] arr = new int[n+1];
+        int i2, i3, i5;
+        i2 = i3 = i5 = 1;
+        arr[1] = 1;
 
-        Long currentUgly = 1L;
-        for (int i = 0; i < n; i++) {
-            currentUgly = uglyNumbersSet.pollFirst(); // Get the smallest number from the set and remove it
+        for(int i=2;i<=n;i++){
+            int i2Ugly = arr[i2] * 2;
+            int i3Ugly = arr[i3] * 3;
+            int i5Ugly = arr[i5] * 5;
+            
+            int minUgly = Math.min(i2Ugly, Math.min(i3Ugly, i5Ugly));
+            arr[i] = minUgly;
 
-            // Insert the next potential ugly numbers into the set
-            uglyNumbersSet.add(currentUgly * 2);
-            uglyNumbersSet.add(currentUgly * 3);
-            uglyNumbersSet.add(currentUgly * 5);
+            if(i2Ugly == minUgly)
+                i2++;
+            if(i3Ugly == minUgly)
+                i3++;
+            if(i5Ugly == minUgly)
+                i5++;
         }
 
-        return currentUgly.intValue(); // Return the nth ugly number
+        return arr[n];
     }
 }
