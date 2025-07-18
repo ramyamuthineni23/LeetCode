59baea17d688184852ff1a6a2f3f43f915eeb4c1
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-07-17 21:21:48 -0500

    Time: 11 ms (17.09%), Space: 55.1 MB (84.25%) - LeetHub

---
+++ 1428-jump-game-iii/1428-jump-game-iii.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            // check if reach zero
+            if (arr[node] == 0) {
+                return true;
+            }
+            if (arr[node] < 0) {
+                continue;
+            }
+
+            // check available next steps
+            if (node + arr[node] < n) {
+                q.offer(node + arr[node]);
+            }
+            if (node - arr[node] >= 0) {
+                q.offer(node - arr[node]);
+            }
+            // mark as visited
+            arr[node] = -arr[node];
+        }
+        return false;
+    }
+}
