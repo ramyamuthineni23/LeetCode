95346c3e6b67f6a3484cac68c8b95435e06db9da
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-08-01 01:03:25 -0500

    Time: 1 ms (91.58%), Space: 42.5 MB (29.12%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,18 +1,12 @@
 class Solution {
-    public int titleToNumber(String s) {
-        int result = 0;
-
-        Map<Character, Integer> alpha_map = new HashMap<>();
-        for (int i = 0; i < 26; i++) {
-            int c = i + 65; // Decimal 65 in ASCII corresponds to char 'A'
-            alpha_map.put((char) c, i + 1);
+    public int titleToNumber(String columnTitle) {
+        int result=0; {
+        for(int i=0;i<columnTitle.length();i++){
+            char temp=columnTitle.charAt(i);
+            int value=temp-'A'+1;
+            result=(result*26)+value;
+            }
+            return result;
         }
-
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            char cur_char = s.charAt(n - 1 - i);
-            result += (alpha_map.get(cur_char) * (Math.pow(26, i)));
-        }
-        return result;
     }
 }
