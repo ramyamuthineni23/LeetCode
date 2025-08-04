d8792c70c10b0bab0919996cf6a9e9c4a940a254
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2025-08-03 22:53:17 -0500

    Time: 9 ms (58.27%), Space: 43.3 MB (96.93%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,39 +1,38 @@
 class Solution {
     public int calculate(String s) {
-        int n=s.length();
-        int sum=0;
-        
-        int sign=1;
-        Stack<Integer> stack=new Stack<>();
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(ch>='0' && ch<='9'){
-                int num=0;
-                while(i<n && (s.charAt(i)>='0' && s.charAt(i)<='9')){
-                    num=num*10+(s.charAt(i)-'0');
-                    i++;
-                }
-                sum+=num*sign;
-                i--;
+        int res = 0;
+        int sign = 1;
+        int num = 0;
+        Stack<Integer> stack = new Stack<>();
 
-            }
-            else if(ch=='+'){
-                sign=1;
-            }
-            else if(ch=='-'){
-                sign=-1;
-            }
-            else if(ch=='('){
-                stack.push(sum);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            } else if (ch == '+') {
+                res += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (ch == '-') {
+                res += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (ch == '(') {
+                stack.push(res);
                 stack.push(sign);
-                sum=0;
-                sign=1;
+                res = 0;
+                sign = 1;
+            } else if (ch == ')') {
+                res += sign * num;
+                num = 0;
+                res *= stack.pop(); // sign
+                res += stack.pop(); // previous result
             }
-            else if(ch==')'){
-                sum=stack.pop()*sum;
-                sum+=stack.pop();
-            }
+            // Ignore spaces
         }
-        return sum;
+
+        return res + sign * num; // Add last pending number
     }
+
 }
